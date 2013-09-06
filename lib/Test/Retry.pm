817cc56178ee5ce9422d02aa96bf18290c267f05
hisaichi5518 <hisada.kazuki@gmail.com>
2013-09-06 09:51:04 +0900

    first commit

---
+++ lib/Test/Retry.pm
@@ -0,0 +1,39 @@
+package Test::Retry;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Retry - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Retry;
+
+=head1 DESCRIPTION
+
+Test::Retry is ...
+
+=head1 LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hisaichi5518 E<lt>hisada.kazuki@gmail.comE<gt>
+
+=cut
+
